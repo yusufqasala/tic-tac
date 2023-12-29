d0d72139ccd56b2d7740d121cd7ba5c4c7dc584f
yousef <yousefmr32004@gmail.com>
2023-12-29 16:09:10 +0200

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setItEditing] = useState(false);
+
+  function handLEeditClick() {
+    setItEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="palyer-name">{playerName}</span>;
+  //   let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = "Save";
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handLEeditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
